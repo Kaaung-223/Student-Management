3aa4fba48e54501a111c25ed1805cc9c37ef5e99
Kaung Min Khant <kk8264483@gmail.com>
2026-09-22 19:39:19 +0630

    update email

--- src/main/java/com/example/student_management_system/Controller/Util/EmailService.java
+++ src/main/java/com/example/student_management_system/Controller/Util/EmailService.java
@@ -23,7 +23,7 @@ public class EmailService {
     //  👇👇👇  EDIT ONLY THESE TWO LINES  👇👇👇
     // ============================================================
     private static final String GMAIL_USER         = "kk8264483@gmail.com";
-    private static final String GMAIL_APP_PASSWORD = "bmqbfcphtbjlqwho";
+    private static final String GMAIL_APP_PASSWORD = "pengtywmyqkzxqqb";
     // ============================================================
     //  👆👆👆  EDIT ONLY THESE TWO LINES  👆👆👆
     // ============================================================
